35447977e4c3b43bbaafa0f487be16a8252f998f
washiner <washiner@gmail.com>
2023-11-08 20:59:40 -0400

    Estudo de Bloc e Cubit atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children:[
             const Text(
               'You have pushed the button this many times:',
             ),
